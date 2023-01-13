2842f2f00517b5899aa5e0c2b87f6d065de7dd37
Tatiana-Hela <malya9472@gmail.com>
2023-01-13 18:44:07 +0300

    complete data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,9 @@
 import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
+
+import { Statistics } from './Statistics/Statistics';
+import data from '../data/data.json';
+
 export const App = () => {
   return (
     <div>
@@ -10,6 +14,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
     </div>
   );
 };
